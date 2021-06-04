e5342d71bc91d83cef8841be0798cc2dac51f07b
Yatskiv Kath <iamkate4@gmail.com>
2021-06-04 19:26:50 +0300

    fixing api

--- api/main.py
+++ api/main.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_restful import Api, Resource
 import json
-import codecs
+import datetime
 
 app = Flask(__name__)
 api = Api(app)
@@ -9,6 +9,8 @@ api = Api(app)
 class LoadAllData(Resource):
     def get(self):
         data = load_data()
+        data = data["tickets"]
+
         return data
 
 class LoadSeats(Resource):
@@ -17,10 +19,10 @@ class LoadSeats(Resource):
 
     def get(self, id):
         data = load_data()
-        print(type(int(id)))
+        data = data["tickets"]
         
         seats = []
-        for train in data["tickets"]:
+        for train in data:
             print(train)
             if train["id"] == int(id):
                 seats = train["trainSeats"]
@@ -28,6 +30,23 @@ class LoadSeats(Resource):
             
         return seats
         
+class LoadByDatePlace(Resource):
+    def get(self, departure, arrive, datetime):
+        data = load_data()
+        data = data["tickets"]
+
+        ids = []
+        for train in data:
+            if(departure not in data["trainCities"]) continue
+            if(arrive not in data["trainCities"]) continue
+            if(data["trainCities"].index(departure) > data["trainCities"].index(arrive)) continue
+
+            departure_index = data["trainCities"].index(departure)
+            arrive_index = data["trainCities"].index(arrive)
+
+        date_time_str = '18/09/19 01:55:19'
+        date_time_obj = datetime.strptime(date_time_str, "%d/%m/%y %H:%M:%S")
+
 api.add_resource(LoadData, "/load_all_data")
 api.add_resource(LoadSeats, '/load_seats/<id>',
                  resource_class_kwargs={'id': id})
